734ea1264ff6dbf9342dd624bf90eaec6f5938f0
Massimo Battestini <massimob76@gmail.com>
2018-10-28 19:33:06 +0000

    cleaning up

--- plugin-core/src/test/java/com/greeting/GreetingPluginTest.java
+++ plugin-core/src/test/java/com/greeting/GreetingPluginTest.java
@@ -1,23 +1,71 @@
 package com.greeting;
 
-
-import org.gradle.api.Project;
-import org.gradle.testfixtures.ProjectBuilder;
+import org.gradle.internal.impldep.org.junit.rules.TemporaryFolder;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GreetingPluginTest {
 
-    @Test
-    void greetingTest(){
-        Project project = ProjectBuilder.builder().build();
-        project.getPluginManager().apply("com.greeting");
+    private final TemporaryFolder testProjectDir = new TemporaryFolder();
 
-        assertTrue(project.getPluginManager().hasPlugin("com.greeting"));
-
-        assertNotNull(project.getTasks().getByName("hello"));
+    @BeforeEach
+    void setUp() throws IOException {
+        testProjectDir.create();
     }
 
+    @AfterEach
+    void cleanUp() {
+        testProjectDir.delete();
+    }
+
+    @Test
+    void greetingTest() throws Exception {
+
+        copyResourceToTestProjectDir("build-gradle");
+
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(testProjectDir.getRoot())
+                .withPluginClasspath()
+                .withArguments("hello")
+                .build();
+
+        assertEquals(SUCCESS, result.task(":hello").getOutcome());
+        assertThat(result.getOutput(), containsString("Hello from GreetingPlugin"));
+
+    }
+
+    @Test
+    void greetingCustomMessageTest() throws Exception {
+
+        copyResourceToTestProjectDir("build-gradle-custom-name");
+
+        BuildResult result = GradleRunner.create()
+                .withProjectDir(testProjectDir.getRoot())
+                .withPluginClasspath()
+                .withArguments("hello")
+                .build();
+
+        assertEquals(SUCCESS, result.task(":hello").getOutcome());
+        assertThat(result.getOutput(), containsString("Hello from Massimo"));
+    }
+
+    private void copyResourceToTestProjectDir(String resourceName) throws URISyntaxException, IOException {
+        Path source = Paths.get(getClass().getClassLoader().getResource(resourceName).toURI());
+        Path destination = Paths.get(testProjectDir.getRoot().toString(), "build.gradle");
+        Files.copy(source, destination);
+    }
 }
